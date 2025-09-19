776864bd736b8ff0680c5341fbbc2d84bd5e9f45
iusmanof <iusmanof@gmail.com>
2025-09-19 05:54:24 +0300

    POST /hometask_01/api/videos v3.2

--- dist/index.js
+++ dist/index.js
@@ -16,61 +16,74 @@ exports.HTTP_STATUS = {
 };
 var Resolutions;
 (function (Resolutions) {
-    Resolutions[Resolutions["P144"] = 0] = "P144";
-    Resolutions[Resolutions["P240"] = 1] = "P240";
-    Resolutions[Resolutions["P360"] = 2] = "P360";
-    Resolutions[Resolutions["P480"] = 3] = "P480";
-    Resolutions[Resolutions["P720"] = 4] = "P720";
-    Resolutions[Resolutions["P1080"] = 5] = "P1080";
-    Resolutions[Resolutions["P1440"] = 6] = "P1440";
-    Resolutions[Resolutions["P2160"] = 7] = "P2160";
+    Resolutions["P144"] = "P144";
+    Resolutions["P240"] = "P240";
+    Resolutions["P360"] = "P360";
+    Resolutions["P480"] = "P480";
+    Resolutions["P720"] = "P720";
+    Resolutions["P1080"] = "P1080";
+    Resolutions["P1440"] = "P1440";
+    Resolutions["P2160"] = "P2160";
 })(Resolutions || (Resolutions = {}));
 // Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)
 const dbVideo = {
-    video: [
-        {
-            id: 1,
-            title: "Video 1",
-            author: "Author 1",
-            canBeDownloaded: true,
-            minAgeRestriction: 18,
-            createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
-            publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-            availableResolutions: [Resolutions.P144, Resolutions.P240]
-        },
-        {
-            id: 2,
-            title: "Video 2",
-            author: "Author 2",
-            canBeDownloaded: true,
-            minAgeRestriction: 7,
-            createdAt: "Fri Sep 19 2025 04:20:20 GMT+0300 (Moscow Standard Time)",
-            publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-            availableResolutions: [Resolutions.P144, Resolutions.P240]
-        },
-        {
-            id: 3,
-            title: "Video 3",
-            author: "Author 3",
-            canBeDownloaded: true,
-            minAgeRestriction: 18,
-            createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
-            publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-            availableResolutions: [Resolutions.P144, Resolutions.P240]
-        }
+    content: [
+    // {
+    //   id: 1,
+    //   title: "Video 1",
+    //   author: "Author 1",
+    //   canBeDownloaded: true,
+    //   minAgeRestriction: 18,
+    //   createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
+    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
+    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
+    // },
+    // {
+    //   id: 2,
+    //   title: "Video 2",
+    //   author: "Author 2",
+    //   canBeDownloaded: true,
+    //   minAgeRestriction: 7,
+    //   createdAt: "Fri Sep 19 2025 04:20:20 GMT+0300 (Moscow Standard Time)",
+    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
+    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
+    // },
+    // {
+    //   id: 3,
+    //   title: "Video 3",
+    //   author: "Author 3",
+    //   canBeDownloaded: true,
+    //   minAgeRestriction: 18,
+    //   createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
+    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
+    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
+    // }
     ]
 };
 app.get('/', (req, res) => {
     res.send('video api');
 });
 app.get('/hometask_01/api/videos', (req, res) => {
-    const foundVideo = dbVideo.video;
+    const foundVideo = dbVideo.content;
     res
         .status(exports.HTTP_STATUS.OK_200)
         .send(foundVideo);
 });
-app.get('/wtf', (req, res) => {
-    res.send(' WTF !');
+app.post('/hometask_01/api/videos', (req, res) => {
+    const createdVideo = {
+        id: Math.floor(Math.random() * 1000000),
+        title: req.body.title,
+        author: req.body.author,
+        canBeDownloaded: false,
+        minAgeRestriction: null,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date(Date.now() + 1).toISOString(),
+        availableResolutions: req.body.availableResolutions
+    };
+    dbVideo.content.push(createdVideo);
+    res
+        .status(exports.HTTP_STATUS.CREATED_201)
+        .json(createdVideo);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -12,51 +12,58 @@ export const HTTP_STATUS = {
   NOT_FOUND_404: 404
 }
 
-enum Resolutions { P144, P240, P360, P480, P720, P1080, P1440, P2160 }
+enum Resolutions { P144= "P144", P240 = "P240", P360 = "P360", P480 = "P480", P720= "P720", P1080="P1080", P1440="P1440", P2160="P2160" }
 
 type videoType = {
   id: number,
   title: string,
   author: string,
   canBeDownloaded: boolean,
-  minAgeRestriction: number
+  minAgeRestriction: number | null,
   createdAt: string,
   publicationDate: string,
   availableResolutions: Resolutions[],
 }
+
+type videoTypeCreate = {
+  "title": string,
+  "author": string,
+  "availableResolutions": Resolutions[],
+}
 // Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)
-const dbVideo: { video: videoType[] } = {
-  video: [
-    {
-      id: 1,
-      title: "Video 1",
-      author: "Author 1",
-      canBeDownloaded: true,
-      minAgeRestriction: 18,
-      createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
-      publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-      availableResolutions: [Resolutions.P144, Resolutions.P240]
-    },
-    {
-      id: 2,
-      title: "Video 2",
-      author: "Author 2",
-      canBeDownloaded: true,
-      minAgeRestriction: 7,
-      createdAt: "Fri Sep 19 2025 04:20:20 GMT+0300 (Moscow Standard Time)",
-      publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-      availableResolutions: [Resolutions.P144, Resolutions.P240]
-    },
-    {
-      id: 3,
-      title: "Video 3",
-      author: "Author 3",
-      canBeDownloaded: true,
-      minAgeRestriction: 18,
-      createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
-      publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-      availableResolutions: [Resolutions.P144, Resolutions.P240]
-    }]
+const dbVideo: { content: videoType[] } = {
+  content: [
+    // {
+    //   id: 1,
+    //   title: "Video 1",
+    //   author: "Author 1",
+    //   canBeDownloaded: true,
+    //   minAgeRestriction: 18,
+    //   createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
+    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
+    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
+    // },
+    // {
+    //   id: 2,
+    //   title: "Video 2",
+    //   author: "Author 2",
+    //   canBeDownloaded: true,
+    //   minAgeRestriction: 7,
+    //   createdAt: "Fri Sep 19 2025 04:20:20 GMT+0300 (Moscow Standard Time)",
+    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
+    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
+    // },
+    // {
+    //   id: 3,
+    //   title: "Video 3",
+    //   author: "Author 3",
+    //   canBeDownloaded: true,
+    //   minAgeRestriction: 18,
+    //   createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
+    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
+    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
+    // }
+    ]
 }
 
 app.get('/', (req: Request, res: Response) => {
@@ -65,15 +72,30 @@ app.get('/', (req: Request, res: Response) => {
 )
 
 app.get('/hometask_01/api/videos', (req: Request, res: Response<videoType[]>) => {
-  const foundVideo: videoType[] = dbVideo.video;
+  const foundVideo: videoType[] = dbVideo.content;
 
   res
     .status(HTTP_STATUS.OK_200)
     .send(foundVideo)
 })
 
-app.get('/wtf', (req: Request, res: Response) => {
-  res.send(' WTF !')
+app.post('/hometask_01/api/videos', (req: Request<{},{}, videoTypeCreate>, res: Response<videoType>) => {
+  const createdVideo: videoType = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    author: req.body.author,
+    canBeDownloaded: false,
+    minAgeRestriction: null,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date(Date.now() + 1).toISOString(),
+    availableResolutions: req.body.availableResolutions
+
+  }
+  dbVideo.content.push(createdVideo)
+
+  res
+    .status(HTTP_STATUS.CREATED_201)
+    .json(createdVideo)
 })
 
 app.listen(port, () => {
