3a9a6f148f20655a142936daf266da132ebdbcd1
iusmanof <iusmanof@gmail.com>
2025-09-19 22:21:02 +0300

    v3.11 ctrl alt L & delete test data

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,16 @@ export const HTTP_STATUS = {
   NOT_FOUND_404: 404
 }
 
-enum Resolutions { P144= "P144", P240 = "P240", P360 = "P360", P480 = "P480", P720= "P720", P1080="P1080", P1440="P1440", P2160="P2160" }
+enum Resolutions {
+  P144 = "P144",
+  P240 = "P240",
+  P360 = "P360",
+  P480 = "P480",
+  P720 = "P720",
+  P1080 = "P1080",
+  P1440 = "P1440",
+  P2160 = "P2160"
+}
 
 type videoType = {
   id: number,
@@ -31,40 +40,8 @@ type videoTypeCreate = {
   "author": string,
   "availableResolutions": Resolutions[],
 }
-// Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)
-let dbVideo: videoType[] =
-  [
-    // {
-    //   id: 1,
-    //   title: "Video 1",
-    //   author: "Author 1",
-    //   canBeDownloaded: true,
-    //   minAgeRestriction: 18,
-    //   createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
-    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
-    // },
-    // {
-    //   id: 2,
-    //   title: "Video 2",
-    //   author: "Author 2",
-    //   canBeDownloaded: true,
-    //   minAgeRestriction: 7,
-    //   createdAt: "Fri Sep 19 2025 04:20:20 GMT+0300 (Moscow Standard Time)",
-    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
-    // },
-    // {
-    //   id: 3,
-    //   title: "Video 3",
-    //   author: "Author 3",
-    //   canBeDownloaded: true,
-    //   minAgeRestriction: 18,
-    //   createdAt: "Fri Sep 19 2025 04:37:29 GMT+0300 (Moscow Standard Time)",
-    //   publicationDate: "Fri Sep 19 2025 05:40:00 GMT+0300 (Moscow Standard Time)",
-    //   availableResolutions: [Resolutions.P144, Resolutions.P240]
-    // }
-    ]
+
+let dbVideo: videoType[] = []
 
 app.get('/', (req: Request, res: Response) => {
     res.send('video api')
@@ -82,8 +59,8 @@ app.get('/videos', (req: Request, res: Response<videoType[]>) => {
     .send(foundVideo)
 })
 
-app.get('/videos/:id', (req: Request<{id: number}>, res: Response) => {
-  const foundVideo : videoType | undefined = dbVideo.find(v => v.id === +req.params.id)
+app.get('/videos/:id', (req: Request<{ id: number }>, res: Response) => {
+  const foundVideo: videoType | undefined = dbVideo.find(v => v.id === +req.params.id)
 
   if (!foundVideo) {
     res.status(HTTP_STATUS.NOT_FOUND_404).send("No video found.")
@@ -91,7 +68,7 @@ app.get('/videos/:id', (req: Request<{id: number}>, res: Response) => {
   res.status(200).json(foundVideo)
 })
 
-app.post('/videos', (req: Request<{},{}, videoTypeCreate>, res: Response<videoType>) => {
+app.post('/videos', (req: Request<{}, {}, videoTypeCreate>, res: Response<videoType>) => {
   const createdVideo: videoType = {
     id: Math.floor(Math.random() * 1000000),
     title: req.body.title,
@@ -103,17 +80,17 @@ app.post('/videos', (req: Request<{},{}, videoTypeCreate>, res: Response<videoTy
     availableResolutions: req.body.availableResolutions
 
   }
-  dbVideo = [ ...dbVideo , createdVideo]
+  dbVideo = [...dbVideo, createdVideo]
   res
     .status(HTTP_STATUS.CREATED_201)
     .json(createdVideo)
 })
 
-app.put('/videos/:id', (req:Request<{id:number},{},videoTypeCreate>, res: Response) =>{
+app.put('/videos/:id', (req: Request<{ id: number }, {}, videoTypeCreate>, res: Response) => {
   const videoInd = dbVideo.findIndex(v => v.id === +req.params.id)
 
-  if(videoInd === -1){
-    res.status(HTTP_STATUS.NOT_FOUND_404).json({message:"Video not found."})
+  if (videoInd === -1) {
+    res.status(HTTP_STATUS.NOT_FOUND_404).json({message: "Video not found."})
   }
 
   const videoUpdate: videoType = {
@@ -123,21 +100,21 @@ app.put('/videos/:id', (req:Request<{id:number},{},videoTypeCreate>, res: Respon
     availableResolutions: req.body.availableResolutions
   }
 
-  dbVideo= [
-    ...dbVideo.slice(0,videoInd),
+  dbVideo = [
+    ...dbVideo.slice(0, videoInd),
     videoUpdate,
-    ...dbVideo.slice(videoInd+1)
+    ...dbVideo.slice(videoInd + 1)
   ]
   res.status(HTTP_STATUS.NO_CONTENT_204).send()
 })
 
-app.delete('/videos/:id', (req:Request<{id: number}>, res: Response) => {
+app.delete('/videos/:id', (req: Request<{ id: number }>, res: Response) => {
   dbVideo = dbVideo.filter(v => v.id !== +req.params.id)
 
   res.status(HTTP_STATUS.NO_CONTENT_204).send()
 })
 
-app.delete('/testing/all-data', (req: Request,res: Response) =>{
+app.delete('/testing/all-data', (req: Request, res: Response) => {
   dbVideo = []
 
   res.status(204).send("All data is deleted")
